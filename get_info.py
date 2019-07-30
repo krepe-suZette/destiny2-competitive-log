fd2dc5f8f83a84eccae883c960889c31c477070b
krepe-suZette <37508580+krepe-suZette@users.noreply.github.com>
2019-07-30 19:22:57 +0900

    Add python source codes
    OME

---
+++ get_info.py
@@ -0,0 +1,170 @@
+import urllib.parse
+import requests
+import json
+import datetime
+
+
+HEADERS = {"X-API-KEY": "076a4b7f8a8746d88f480a0c994b8b54"}
+
+raceType = {0: "인간", 1: "각성자", 2: "엑소"}
+classType = {0: "타이탄", 1: "헌터", 2: "워록"}
+activityModeType = {37: "생존", 38: "카운트다운", 72: "격돌", 74: "점령"}
+
+
+def period2datetime(period):
+    return datetime.datetime.strptime(period, "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(hours=9)
+
+
+def split_season(date):
+    # 귀찮으니 미구현
+    return "7"
+
+
+def change_data(data, glory):
+    activity_time = period2datetime(data["period"])
+    info = {
+        "instanceId": data['activityDetails']['instanceId'],
+        "time": activity_time.__str__(),
+        "mode": activityModeType.get(data["activityDetails"]["mode"], data["activityDetails"]["mode"]),
+        "kda": f"{data['values']['kills']['basic']['displayValue']}/{data['values']['deaths']['basic']['displayValue']}/{data['values']['assists']['basic']['displayValue']}",
+        "efficiency": data['values']['efficiency']['basic']['displayValue'],
+        "glory": glory
+    }
+    return info
+
+
+def find(data: list, instanceId):
+    for info in data:
+        if info["instanceId"] == instanceId:
+            return True
+        else:
+            continue
+    return False
+
+
+def save(data, glory, path):
+    with open(path, "r", encoding="utf-8") as f:
+        ori = json.load(f)
+    info = change_data(data, glory)
+    activity_time = datetime.datetime.strptime(info["time"], "%Y-%m-%d %H:%M:%S")
+    if not ori.get(f"{activity_time.year}/{activity_time.month}/{activity_time.day}"):
+        ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"] = [info]
+    else:
+        if find(ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"], info["instanceId"]):
+            # override recent data
+            ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"][-1] = info
+        else:
+            ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"].append(info)
+    print("{instanceId}, {time}, {mode}, {kda}, {efficiency}".format(**info))
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(ori, f, indent=2, ensure_ascii=False)
+    return
+
+
+def save_many(data, path, reset=True):
+    if reset:
+        ori = {}
+    else:
+        with open(path, "r", encoding="utf-8") as f:
+            ori = json.load(f)
+    for info in data:
+        activity_time = datetime.datetime.strptime(info["time"], "%Y-%m-%d %H:%M:%S")
+        if not ori.get(f"{activity_time.year}/{activity_time.month}/{activity_time.day}"):
+            ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"] = [info]
+        else:
+            ori[f"{activity_time.year}/{activity_time.month}/{activity_time.day}"].append(info)
+        print("{instanceId}, {time}, {mode}, {kda}, {efficiency}".format(**info))
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(ori, f, indent=2, ensure_ascii=False)
+    return
+
+
+def make_info_object(data, glory):
+    activity_time = period2datetime(data["period"])
+    info = {
+        "instanceId": data['activityDetails']['instanceId'],
+        "time": activity_time.__str__(),
+        "mode": activityModeType.get(data["activityDetails"]["mode"], data["activityDetails"]["mode"]),
+        "kda": f"{data['values']['kills']['basic']['displayValue']}/{data['values']['deaths']['basic']['displayValue']}/{data['values']['assists']['basic']['displayValue']}",
+        "efficiency": data['values']['efficiency']['basic']['displayValue'],
+        "glory": glory if glory else None
+    }
+    return info
+
+
+def bungie_response_wrapper(original_func):
+    def wrapper_func(*args, **kwargs):
+        resp = original_func(*args, **kwargs).json()
+        if not resp.get("Response"):
+            raise Exception(f"No response on function {original_func.__name__}")
+        return resp.get("Response")
+    return wrapper_func
+
+
+@bungie_response_wrapper
+def search_destiny_player(displayName):
+    return requests.get(f"https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/{urllib.parse.quote(displayName)}", headers=HEADERS)
+
+
+@bungie_response_wrapper
+def get_profile(membershipType, membershipId, components):
+    return requests.get(f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Profile/{membershipId}/?components={','.join(components)}", headers=HEADERS)
+
+
+@bungie_response_wrapper
+def get_activity_history(membershipType, membershipId, characterId, count=3, mode=0, page=0):
+    data = {"count": count, "mode": mode, "page": page}
+    return requests.get(f"https://www.bungie.net/Platform/Destiny2/{membershipType}/Account/{membershipId}/Character/{characterId}/Stats/Activities/", headers=HEADERS, params=data)
+
+
+def update(displayName):
+    _player_info = search_destiny_player(displayName)[0]
+    membershipId = _player_info["membershipId"]
+    membershipType = _player_info["membershipType"]
+    _characters_data = get_profile(membershipType, membershipId, ['200', '202'])
+    _characters_id_list = list(_characters_data["characters"]["data"].keys())
+    _glory = _characters_data["characterProgressions"]["data"][_characters_id_list[0]]["progressions"]["2679551909"]["currentProgress"]
+    for characterId in _characters_id_list:
+        _recent_activity = get_activity_history(membershipType, membershipId, characterId, count=1, mode=69)
+        _activity_time = period2datetime(_recent_activity.get("activities")[0].get("period")) + datetime.timedelta(hours=9)
+        if _activity_time < datetime.datetime(2019, 7, 1):
+            print("19년 7월 1일 이전의 기록입니다.")
+            continue
+        save(_recent_activity.get("activities")[0], _glory, "crucible_data.json")
+        print(_activity_time)
+        print(_recent_activity.get("activities")[0].get("activityDetails").get("instanceId"))
+    return
+
+
+def initializing_all(displayName):
+    _player_info = search_destiny_player(displayName)[0]
+    membershipId = _player_info["membershipId"]
+    membershipType = _player_info["membershipType"]
+    _characters_data = get_profile(membershipType, membershipId, ['200', '202'])
+    _characters_id_list = list(_characters_data["characters"]["data"].keys())
+    _info_list = []
+    # 캐릭터별로 최근 30개의 정보 받아오기
+    for characterId in _characters_id_list:
+        # 경쟁 플레이리스트 기록 (최대 30개) -> list
+        _recent_activities = get_activity_history(membershipType, membershipId, characterId, count=100, mode=69).get("activities")
+        for _recent_activity in _recent_activities:
+            _activity_time = period2datetime(_recent_activity.get("period")) + datetime.timedelta(hours=9)
+            # 특정 날짜 이전 기록은 스킵
+            if _activity_time < datetime.datetime(2019, 6, 5):
+                print(f"- 7시즌 이전 기록은 건너뜁니다. {_activity_time}")
+                continue
+            _info_list.append(change_data(_recent_activity, ""))
+    _info_list.sort(key=lambda x: x["time"])
+    save_many(_info_list, "crucible_data.json", True)
+    return
+
+
+def main():
+    with open("player_name", "r", encoding="utf-8") as f:
+        player_name = f.read()
+    initializing_all(player_name)
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ routes.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import json
+
+import get_info
+
+
+with open("player_name", "r", encoding="utf-8") as f:
+    PLAYER_NAME = f.read()
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    with open("crucible_data.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    with open("daily_review.json", "r", encoding="utf-8") as f:
+        review = json.load(f)
+    return render_template("root.html", data=data, review=review)
+
+
+@app.route("/update")
+def update():
+    get_info.update(PLAYER_NAME)
+    return "<script language='javascript'>self.close();</script>"
+
+
+@app.route("/review")
+def review_update():
+    date = request.args.get("date")
+    with open("daily_review.json", "r", encoding="utf-8") as f:
+        review = json.load(f)
+    return render_template("review.html", date=date, data=review.get(date, ""))
+
+
+@app.route('/review/post', methods=["POST"])
+def review_post():
+    # review, date
+    with open("daily_review.json", "r", encoding="utf-8") as f:
+        review = json.load(f)
+    review[request.form["date"]] = request.form["review"]
+    with open("daily_review.json", "w", encoding="utf-8") as f:
+        json.dump(review, f, ensure_ascii=False, indent=2)
+    return "<script language='javascript'>self.close();</script>"
+
+
+@app.route("/initialize")
+def initialize():
+    get_info.initializing_all(PLAYER_NAME)
+    get_info.update(PLAYER_NAME)
+    return "<script language='javascript'>self.close();</script>"
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
